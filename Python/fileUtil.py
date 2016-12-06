43c7538cf8e663c360d4609940ca6eeed5d9b3f4
luther168 <1554140485@qq.com>
2016-12-06 14:36:56 +0800

    Update fileUtil.py

--- Python/fileUtil.py
+++ Python/fileUtil.py
@@ -1,1 +1,33 @@
+#-*- coding:UTF-8 -*-
+
+import os
+
+symbol = '\t'
+
+#递归遍历指定目录，返回目录下的所有文件名
+def eachFile(filepath, flagCount = 0):
+    pathDir = os.listdir(filepath)
+    
+    flag =""
+    for i in range(flagCount):
+        flag = flag + symbol
+    
+    strList = flag
+    if flagCount == 0:
+        strList = filepath + os.linesep
+    else:
+        strList = flag + filepath.split(os.path.sep)[-2] + os.linesep
+    flag = flag + symbol
+    
+    for allDir in pathDir:
+        child = os.path.join('%s%s' % (filepath, allDir))
+        if os.path.isdir(child):
+            strList = strList + eachFile(child + os.path.sep, flagCount + 1)
+        else:
+            strList = strList + flag + allDir + os.linesep
+    return strList
+        
+if __name__=='__main__':
+    filepath='F:' + os.path.sep + 'software' + os.path.sep
+    print(eachFile(filepath))
 
