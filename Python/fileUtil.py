26ba4377246d18322c2838f903340ac68621310c
Luther <1554140485@qq.com>
2017-05-06 14:25:16 +0800

    重构列举目录所有文件的方法

--- Python/fileUtil.py
+++ Python/fileUtil.py
@@ -2,32 +2,23 @@
 
 import os
 
-symbol = '\t'
+symbol = '- '
 
 #递归遍历指定目录，返回目录下的所有文件名
-def eachFile(filepath, flagCount = 0):
-    pathDir = os.listdir(filepath)
-    
-    flag =""
-    for i in range(flagCount):
-        flag = flag + symbol
-    
-    strList = flag
-    if flagCount == 0:
-        strList = filepath + os.linesep
-    else:
-        strList = flag + filepath.split(os.path.sep)[-2] + os.linesep
+def eachFile(filepath, flag = ''):
+    childFiles = os.listdir(filepath)    
+    strList = flag + filepath.split(os.path.sep)[-2] + os.linesep
     flag = flag + symbol
-    
-    for allDir in pathDir:
-        child = os.path.join('%s%s' % (filepath, allDir))
+    for childFile in childFiles:
+        child = os.path.join('%s%s' % (filepath, childFile))
         if os.path.isdir(child):
-            strList = strList + eachFile(child + os.path.sep, flagCount + 1)
+            strList = strList + eachFile(child + os.path.sep, flag)
         else:
-            strList = strList + flag + allDir + os.linesep
+            strList = strList + flag + childFile + os.linesep
     return strList
         
 if __name__=='__main__':
-    filepath='F:' + os.path.sep + 'software' + os.path.sep
-    print(eachFile(filepath))
+    parentPath = 'F:' + os.path.sep
+    filepath= parentPath + 'software' + os.path.sep
+    print(parentPath + eachFile(filepath))
 
