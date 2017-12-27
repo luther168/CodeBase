4c4feec169a57e2037874e9c331cc36c8a6a0c00
Hurson_ht <1554140485@qq.com>
2017-12-27 14:23:01 +0800

    Refactor the code

--- JavaWeb/MavenSSM/src/main/java/cn/luo/mybatis/dao/mapper/SQLiteMapper.java
+++ JavaWeb/MavenSSM/src/main/java/cn/luo/mybatis/dao/mapper/SQLiteMapper.java
@@ -1,11 +1,19 @@
 package cn.luo.mybatis.dao.mapper;
 
-import org.apache.ibatis.annotations.Select;
+import cn.luo.mybatis.base.BaseMapper;
+import cn.luo.mybatis.provider.CommonProvider;
+import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.List;
 import java.util.Map;
 
-public interface SQLiteMapper {
-    @Select("select * from UserInfo")
-    List<Map<String,Object>> getList();
+public interface SQLiteMapper extends BaseMapper {
+
+    @Override
+    @SelectProvider(type = CommonProvider.class, method = "select")
+    List<Map<String, Object>> select(String selectStr, String tableName, String whereStr, int limit, int offset);
+
+    @Override
+    @SelectProvider(type = CommonProvider.class, method = "selectAll")
+    List<Map<String, Object>> selectAll(String tableName);
 }
--- JavaWeb/MavenSSM/src/test/java/com/luo/test/MybatisTest.java
+++ JavaWeb/MavenSSM/src/test/java/com/luo/test/MybatisTest.java
@@ -1,36 +1,18 @@
 package com.luo.test;
 
-import cn.luo.mybatis.MultipleDataSource;
-import cn.luo.mybatis.dao.mapper.MySqlMapper;
-import cn.luo.mybatis.dao.mapper.SQLiteMapper;
-import cn.luo.mybatis.dao.mapper.SqlServerMapper;
+import cn.luo.mybatis.DBHelper;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 import java.util.Map;
 
 public class MybatisTest {
 
+    private DBHelper dbHelper = DBHelper.getInstance();
+
     @Test
-    public void getList() {
-        //初始化ApplicationContext
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-mybatis.xml");
-
-//        MySqlMapper mySqlMapper = applicationContext.getBean(MySqlMapper.class);
-//        SqlServerMapper sqlServerMapper = applicationContext.getBean(SqlServerMapper.class);
-        SQLiteMapper sqLiteMapper = applicationContext.getBean(SQLiteMapper.class);
-
-        //设置数据源为MySql,使用了AOP测试时请将下面这行注释
-//        MultipleDataSource.setDataSourceKey("mySqlDataSource");
-//        mySqlMapper.getList();
-        //设置数据源为SqlServer,使用AOP测试时请将下面这行注释
-//        MultipleDataSource.setDataSourceKey("sqlServerDataSource");
-//        sqlServerMapper.getList();
-        //设置数据源为SQLite,使用AOP测试时请将下面这行注释
-        MultipleDataSource.setDataSourceKey("sqliteDataSource");
-        List<Map<String, Object>> list = sqLiteMapper.getList();
+    public void find() {
+        List<Map<String, Object>> list = dbHelper.select("UserInfo");
         System.out.println(list);
     }
 }
